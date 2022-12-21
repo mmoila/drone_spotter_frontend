e414fc896d8cb0018211a62cbe818fa9f540b261
Mikael Moilanen <mikael.moilanen@gmail.com>
2022-12-21 17:50:41 +0200

    Add rest of the owner data to dronelist

--- src/components/DroneList.js
+++ src/components/DroneList.js
@@ -6,29 +6,40 @@ import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
 
 const DroneList = ({ drones }) => {
+  const formatTime = (timeString) => {
+    const time = new Date(timeString)
+    return time.toUTCString().slice(0, -7)
+  }
+
   return (
     <TableContainer component={Paper} sx={{ marginTop: 5 }}>
       <Typography variant="h3" component={"h1"} m={5} align={"center"}>
         No Drone Zone intrudes
       </Typography>
-      <Table size="medium">
+      <Table>
         <TableHead>
           <TableRow sx={{ fontWeight: "bold" }}>
-            <TableCell align="center">Latest Observation Time</TableCell>
+            <TableCell align="center">Latest Observation Time (UTC)</TableCell>
             <TableCell align="center">Serial Number</TableCell>
             <TableCell align="center">Closest Distance To The Nest</TableCell>
             <TableCell align="center">Drone owner</TableCell>
+            <TableCell align="center">Owner email</TableCell>
+            <TableCell align="center">Owner phone</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {drones.map((drone) => (
             <TableRow key={drone.serialNumber}>
-              <TableCell align="center">{drone.timeStamp}</TableCell>
+              <TableCell align="center">
+                {formatTime(drone.timeStamp)}
+              </TableCell>
               <TableCell align="center">{drone.serialNumber}</TableCell>
               <TableCell align="center">
                 {`${(drone.closestDistance / 1000).toFixed(1)} m`}
               </TableCell>
               <TableCell align="center">{drone.owner.name}</TableCell>
+              <TableCell align="center">{drone.owner.email}</TableCell>
+              <TableCell align="center">{drone.owner.phoneNumber}</TableCell>
             </TableRow>
           ))}
         </TableBody>
