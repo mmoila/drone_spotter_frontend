288631f2af965f01e7fab000257cfcba7f72d715
Mikael Moilanen <mikael.moilanen@gmail.com>
2023-01-05 11:26:28 +0200

    Rescale dronemap and add NDZ text

--- src/components/DroneMap.js
+++ src/components/DroneMap.js
@@ -1,4 +1,4 @@
-import { Circle, Stage, Layer, Group } from "react-konva"
+import { Circle, Stage, Layer, Group, Text } from "react-konva"
 import { Paper, Container, Box } from "@mui/material"
 import { useRef } from "react"
 import DroneMarker from "./DroneMarker"
@@ -6,6 +6,21 @@ import DroneMarker from "./DroneMarker"
 const DroneMap = ({ drones }) => {
   const ndzCircle = useRef()
   const nest = useRef()
+  const wholeWidth = 500
+  const scaleMultiplier = 2.5
+
+  const calculateScaledPosition = (originalPos) => {
+    const nestCoordinates = [250, 250]
+    const orgXFromNest = originalPos[0] / 1000 - nestCoordinates[0]
+    const orgYFromNest = originalPos[1] / 1000 - nestCoordinates[1]
+    const newXFromNest = orgXFromNest * scaleMultiplier
+    const newYFromNest = orgYFromNest * scaleMultiplier
+
+    return [
+      nestCoordinates[0] + newXFromNest,
+      nestCoordinates[1] + newYFromNest,
+    ]
+  }
 
   return (
     <Container
@@ -15,39 +30,49 @@ const DroneMap = ({ drones }) => {
         justifyContent: "center",
       }}
     >
-      <Box width={500} height={500}>
+      <Box width={wholeWidth} height={wholeWidth}>
         <Paper
           variant="outlined"
-          width={0.5}
+          width={1}
           height={1}
           sx={{ borderColor: "black", backgroundColor: "#white" }}
         >
-          <Stage width={500} height={500}>
+          <Stage width={wholeWidth} height={wholeWidth}>
             <Layer>
               <Group>
                 <Circle
                   ref={nest}
                   radius={2.5}
                   fill="black"
-                  x={250}
-                  y={250}
-                ></Circle>
+                  x={wholeWidth / 2}
+                  y={wholeWidth / 2}
+                />
+                <Text
+                  x={wholeWidth / 9}
+                  y={wholeWidth / 8}
+                  rotation={-40}
+                  text="NDZ"
+                  fontSize={22}
+                  fontStyle="bold"
+                />
                 <Circle
                   ref={ndzCircle}
-                  radius={250}
-                  x={250}
-                  y={250}
+                  radius={100 * scaleMultiplier}
+                  x={wholeWidth / 2}
+                  y={wholeWidth / 2}
                   fill="Red"
                   opacity={0.3}
                   strokeWidth={5}
                   stroke="white"
                 ></Circle>
                 {drones.map((drone) => {
+                  const pos = calculateScaledPosition(drone.position)
+
                   return (
                     <DroneMarker
                       key={drone.serialNumber}
-                      xPos={drone.position[0] / 1000}
-                      yPos={drone.position[1] / 1000}
+                      xPos={pos[0]}
+                      yPos={pos[1]}
                     />
                   )
                 })}
